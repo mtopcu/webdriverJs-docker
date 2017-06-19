39dd6f904ac87bed649593598f6296d747cac006
Mustafa <mtopcuogullari@innova.com.tr>
2017-06-19 16:06:14 +0300

    Update wpdocker.js
    password

--- workspace/wpdocker.js
+++ workspace/wpdocker.js
@@ -15,6 +15,7 @@ driver.findElement(By.id('language-continue')).click();
 driver.wait(until.elementLocated(By.id('weblog_title')), 10000);
 driver.findElement(By.id('weblog_title')).sendKeys('Test');
 driver.findElement(By.id('user_login')).sendKeys('mtopcu');
+driver.findElement(By.id('pass1-text')).sendKeys('W99887766p');
 driver.findElement(By.id('admin_email')).sendKeys('mtopcu@test.com');
 driver.findElement(By.id('submit')).click();
 
