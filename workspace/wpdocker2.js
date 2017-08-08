2dc34fef52d8caac2d0191266b963a32360934ac
Mustafa <mtopcuogullari@innova.com.tr>
2017-08-08 10:27:54 +0300

    Update wpdocker2.js
    update

--- workspace/wpdocker2.js
+++ workspace/wpdocker2.js
@@ -1,5 +1,5 @@
 var webdriver = require('selenium-webdriver');
-var webdriver = require('selenium-webdriver');
+
 var assert = require('assert');
 var By = webdriver.By;
 var until = webdriver.until;
@@ -9,5 +9,15 @@ usingServer('http://hub:4444/wd/hub').
 withCapabilities(webdriver.Capabilities.chrome()).build();
 driver.get('http://testpress:80/');
 
-driver.sleep(15000);
+driver.getTitle().then(function(title){
+	console.log('The page title is ->',title);
+  
+driver.sleep(5000);  
+
+driver.findElement(By.id('logo')).click(); 
+  
+driver.getTitle().then(function(title){
+	console.log('The page title is ->',title);
+
+driver.sleep(10000);
 driver.quit();
